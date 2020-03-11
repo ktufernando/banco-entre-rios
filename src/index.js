be4e916e40d91fbd21bac64fbeaf1c2b0e5f5200
ktufernando <ktu@fervaldes.com>
2020-03-11 11:03:38 -0300

    error handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan');
-const swaggerjsDocs = require('swagger-jsdoc');
+const swaggerJsDocs = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const errorhandler = require('errorhandler');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 
 // ---> Setting
@@ -18,17 +21,17 @@ const swaggerOptions = {
             contact:{
                 name: "Banco Entre Rios"
             },
-            servers: ["http://ñpcalhost:3000"]
+            servers: ["http://localhost:3000"]
         }
     },
     apis: ['src/index.js']
 };
 
-const swaggerDocs = swaggerjsDocs(swaggerOptions);
+const swaggerDocs = swaggerJsDocs(swaggerOptions);
 
 
 
-// ---> Middlewares
+// ---> Middleware
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -42,9 +45,9 @@ app.use(require('./routes/index'));
  * /customers:
  *  get:
  *      description: Use to request all customers
- *      reponses: 
+ *      responses: 
  *          '200':
- *             description: A succeful response
+ *             description: A successful response
  */
 app.get("/customers", (req, res) => {
     console.log("request");
@@ -56,18 +59,56 @@ app.get("/customers", (req, res) => {
  * /customer:
  *  put:
  *      description: Use to update a customers
- *      reponses: 
+ *      responses: 
  *          '201':
- *             description: A succeful response
+ *             description: A successful response
  */
 app.put("/customer", (req, res) => {
     console.log("request");
-    res.status(201).send("Succssefully update customer"); 
+    res.status(201).send("Successfully update customer"); 
 });
 
+if (!isProduction) {
+    app.use(errorhandler());
+}
 
+// ---> Catch 404 and forward to error handler
+app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// ---> Error handlers
+if (!isProduction) {
+    // Development error handler
+    // will print stacktrace
+    app.use((err, req, res, next) => {
+        console.log(err.stack);
+
+        res.status(err.status || 500);
+
+        res.json({
+            errors: {
+                message: err.message,
+                error: err
+            }
+        });
+    });
+}else {
+    // production error handler
+    // no stacktrace leaked to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            errors: {
+                message: err.message
+            }
+        });
+    });
+}
 
 // ---> Starting the server
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`)
+    console.log(`Server is listening on port ${app.get('port')}`)
 })
