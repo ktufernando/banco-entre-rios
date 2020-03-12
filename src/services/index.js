f05795cb994465efa5873ccb11ddfe207ecafc68
ktufernando <ktu@fervaldes.com>
2020-03-12 15:03:34 -0300

    fix read file from filesystem

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,12 @@
 const axios = require('axios');
+const fs = require("fs");
+const path = require('path');
 
 const getData =  async () => {
 
-    try{
-        const res = await axios.get('http://localhost:3000/v1/ber/datamock');
-        const data = res.data;
-        return data;
-    } catch(error){
-        const msg = 'There was an error trying to get the data';
-        throw new Error(msg, error);
-    }
+        const data = fs.readFileSync(path.resolve(__dirname, '../data/dataMock.js'));
+        console.log(data);
+        return JSON.parse(data);
 }
 
 
