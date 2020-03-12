8c191c0dddfde54351b132344a8361696498a684
roycuenca <roycuenca2013@gmail.com>
2020-03-12 14:21:15 -0300

    fix in object datamock file

---
+++ dataMock.js
@@ -0,0 +1,365 @@
+[
+    {
+        'proveedorTelefonoMovil': [
+            {
+                value: 'cti',
+                label: 'Claro',
+            },
+            {
+                value: 'movistar',
+                label: 'Movistar',
+            },
+            {
+                value: 'nextel',
+                label: 'Nextel',
+            },
+            {
+                value: 'personal',
+                label: 'Personal',
+            }
+        ],
+        'provincia': [
+            {
+                value: '1',
+                label: 'Capital Federal',
+            },
+            {
+                value: '2',
+                label: 'Buenos Aires',
+            },
+            {
+                value: '3',
+                label: 'CATAMARCA',
+            },
+            {
+                value: '4',
+                label: 'CORDOBA',
+            },
+            {
+                value: '5',
+                label: 'CORRIENTES',
+            },
+            {
+                value: '6',
+                label: 'CHACO',
+            },
+            {
+                value: '7',
+                label: 'CHUBUT',
+            },
+            {
+                value: '8',
+                label: 'ENTRE RIOS',
+            },
+            {
+                value: '9',
+                label: 'FARMOSA',
+            },
+            {
+                value: '10',
+                label: 'JUJUY',
+            },
+            {
+                value: '11',
+                label: 'LA PAMPA',
+            },
+            {
+                value: '12',
+                label: 'LA RIOJA',
+            },
+            {
+                value: '13',
+                label: 'MENDOZA',
+            },
+            {
+                value: '14',
+                label: 'MISIONES',
+            },
+            {
+                value: '15',
+                label: 'NEUQUEN',
+            },
+            {
+                value: '16',
+                label: 'RIO NEGRO',
+            },
+            {
+                value: '17',
+                label: 'SALTA',
+            },
+            {
+                value: '18',
+                label: 'SAN JUAN',
+            },
+            {
+                value: '19',
+                label: 'SAN LUIS',
+            },
+            {
+                value: '20',
+                label: 'SANTA CRUZ',
+            },
+            {
+                value: '21',
+                label: 'SANTA FE',
+            },
+            {
+                value: '22',
+                label: 'SANTIAGO DEL ESTERO',
+            },
+            {
+                value: '23',
+                label: 'TUCUMAN',
+            },
+            {
+                value: '24',
+                label: 'TIERRA DEL FUEGO, ANTARTUDA E ISLAS',
+            },
+        ],
+        'clienteBancario': [
+            {
+                value: true,
+                label: 'SI',
+            },
+            {
+                value: false,
+                label: 'NO',
+            }
+        ],
+        'montoMaximoParaSolicitar': 'number',
+        'actividad': [
+            // ---> Agropecuario <---
+            {
+                value: '2',
+                label: 'Agropecuario',
+                destino: [
+                    {
+                        value: '13',
+                        label: 'Adquisición de Camiones y Acoplados',
+                        garantia: [
+                            {
+                                value: '13',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '14',
+                        label: 'Maquinas Agricolas',
+                        garantia: [
+                            {
+                                value: '14',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '15',
+                        label: 'Pick Up y Rodados',
+                        garantia: [
+                            {
+                                value: '15',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '16',
+                        label: 'Leasing Rodados',
+                        garantia: [
+                            {
+                                value: '16',
+                                label: 'Leasing',
+                            }
+                        ]
+                    },
+                    {
+                        value: '17',
+                        label: 'Compra y/o Retención de Hacienda (Hasta $1.500.000)',
+                        garantia: [
+                            {
+                                value: '17',
+                                label: 'Sola Firma / Fianza',
+                            }
+                        ]
+                    },
+                    {
+                        value: '18',
+                        label: 'Insumos siembra fina y/o gruesa (Hasta $1.500.000)',
+                        garantia: [
+                            {
+                                value: '18',
+                                label: 'Sola Firma / Fianza',
+                            }
+                        ]
+                    },
+                    {
+                        value: '19',
+                        label: 'Insumos para Tambo (Hasta $1.500.000)',
+                        garantia: [
+                            {
+                                value: '19',
+                                label: 'Sola Firma / Fianza',
+                            }
+                        ]
+                    },
+                ]
+            },
+            // ---> Comercios <---    
+            {
+                value: '3',
+                label: 'Comercio',
+                destino: [
+                    {
+                        value: '32',
+                        label: 'Adquisición de Camiones y Acoplados',
+                        garantia: [
+                            {
+                                value: '32',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '33',
+                        label: 'Utilitarios',
+                        garantia: [
+                            {
+                                value: '33',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '34',
+                        label: 'Pick Up y Rodados',
+                        garantia: [
+                            {
+                                value: '34',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '35',
+                        label: 'Leasing Rodados',
+                        garantia: [
+                            {
+                                value: '35',
+                                label: 'Leasing',
+                            }
+                        ]
+                    },
+                    {
+                        value: '36',
+                        label: 'Maquinas y Equipos',
+                        garantia: [
+                            {
+                                value: '36',
+                                label: 'Hipotecaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '37',
+                        label: 'Capital de Trabajo (Hasta $1.500.000)',
+                        garantia: [
+                            {
+                                value: '37',
+                                label: 'Sola Firma / Fianza',
+                            }
+                        ]
+                    },
+                    {
+                        value: '38',
+                        label: 'ICapital de Trabajo (Mayor $1.500.000)',
+                        garantia: [
+                            {
+                                value: '38',
+                                label: 'Hipotecaria',
+                            }
+                        ]
+                    },
+                ]
+            },
+                // ---> Servicios <---
+
+            {
+                value: '4',
+                label: 'Servicios',
+                destino: [
+                    {
+                        value: '47',
+                        label: 'Adquisición de Camiones y Acoplados',
+                        garantia: [
+                            {
+                                value: '47',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '48',
+                        label: 'Utilitarios',
+                        garantia: [
+                            {
+                                value: '48',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '49',
+                        label: 'Pick Up y Rodados',
+                        garantia: [
+                            {
+                                value: '49',
+                                label: 'Prendaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '50',
+                        label: 'Leasing Rodados',
+                        garantia: [
+                            {
+                                value: '50',
+                                label: 'Leasing',
+                            }
+                        ]
+                    },
+                    {
+                        value: '51',
+                        label: 'Maquinas y Equipos',
+                        garantia: [
+                            {
+                                value: '51',
+                                label: 'Hipotecaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '52',
+                        label: 'Maquinas y Equipos Viales',
+                        garantia: [
+                            {
+                                value: '52',
+                                label: 'Hipotecaria',
+                            }
+                        ]
+                    },
+                    {
+                        value: '53',
+                        label: 'Capital de Trabajo (Hasta $1.500.000)',
+                        garantia: [
+                            {
+                                value: '53',
+                                label: 'Sola Firma / Fianza',
+                            }
+                        ]
+                    },
+                ]
+            },
+        ]
+    }
+]
