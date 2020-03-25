dccef097e617e5e208a85cc7784a7dd8ef43f26a
roy.cuenca <roy.cuenca@semperti.local>
2020-03-25 13:54:04 -0300

    added qrRoutes & qrServices

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const pyme = require('./routes/pyme');
+const qr = require('./routes/qr');
+
 
 // guaranteed to Get dependencies
 module.exports = () => {
 	const app = Router();
 	pyme(app);
+	qr(app);
+
 
 	return app
 }
---
+++ src/api/routes/qr.js
@@ -0,0 +1,67 @@
+const logger = require('../../loaders/logger');
+const { handleSuccessResponse } = require('../../utils/ResponseHandler');
+const { Router } = require('express');
+const router = Router();
+const path = require('path');
+const { getCuil,
+        getMaritalState,
+        getCountries,
+        getValidateBirdtDay,
+        getLocalities
+    } = require('../../services/qrServices');
+
+module.exports = (app) => {
+    app.use('/qr', router);
+
+
+    // --> GET Cuil
+    router.get('/chargeWithQR/getCUIL/:dni', async (req, res, next) => {
+        logger.silly('Entrada de endpoint para obtener codigo qr');
+        try {
+            res.status(200).json(handleSuccessResponse(await getCuil()));
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    // --> GET Marital State
+    router.get('/chargeWithQR/getEstadoCivil', async (req, res, next) => {
+        logger.silly('Entrada de endpoint para obtener estado civil');
+        try {
+            res.status(200).json(handleSuccessResponse(await getMaritalState()));
+        } catch (error) {
+            next(error);
+        }
+    });
+    
+    // --> GET Countries
+    router.get('/chargeWithQR/getCountries', async (req, res, next) => {
+        logger.silly('Entrada de endpoint para obtener paises');
+        try {
+            res.status(200).json(handleSuccessResponse(await getCountries()));
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    // --> GET date of birth
+    router.get('/chargeWithQR/validateApplicantAge/:applicantDayOfBirth', async (req, res, next) => {
+        logger.silly('Entrada de endpoint para obtener paises');
+        try {
+            res.status(200).json(handleSuccessResponse(await getValidateBirdtDay()));
+        } catch (error) {
+            next(error);
+        }
+    });
+    
+    // --> GET Localities
+    router.get('/chargeWithQR/getLocalities/:postalCode', async (req, res, next) => {
+        logger.silly('Entrada de endpoint para obtener paises');
+        try {
+            res.status(200).json(handleSuccessResponse(await getLocalities()));
+        } catch (error) {
+            next(error);
+        }
+    });
+
+};
